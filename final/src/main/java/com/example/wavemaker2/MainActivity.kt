a8a37471c72c97b032329d1f47d2f22fc4f1307f
steve-zxc <zxc.steve@gmail.com>
2023-03-11 17:36:23 +0800

    working Kotlin Before try new Compose UI

--- final/src/main/java/com/example/wavemaker2/MainActivity.kt
+++ final/src/main/java/com/example/wavemaker2/MainActivity.kt
@@ -30,6 +30,7 @@ import androidx.core.content.PermissionChecker
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */   class MainActivity : AppCompatActivity() {
+    val name="Compose UI"
     external fun startEngine()
     external fun stopEngine()
     external fun setRecording(isRecording: Boolean)
